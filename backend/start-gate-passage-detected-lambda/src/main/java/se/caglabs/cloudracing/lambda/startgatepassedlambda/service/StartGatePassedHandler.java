6d43e12becde1d0e59258143892d99492896451e
Stefan Pilakouris <stefan.pilakouris@arbetsformedlingen.se>
2019-11-07 17:26:08 +0100

    Updated start-gate-passage-detected-lambda

--- backend/start-gate-passage-detected-lambda/src/main/java/se/caglabs/cloudracing/lambda/startgatepassedlambda/service/StartGatePassedHandler.java
+++ backend/start-gate-passage-detected-lambda/src/main/java/se/caglabs/cloudracing/lambda/startgatepassedlambda/service/StartGatePassedHandler.java
@@ -3,9 +3,32 @@ package se.caglabs.cloudracing.lambda.startgatepassedlambda.service;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.sql.Timestamp;
+
 
 public class StartGatePassedHandler {
     public APIGatewayProxyResponseEvent execute(APIGatewayProxyRequestEvent request, Context context) {
-        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hello  all");
+        //return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hello  all");
+
+        String timestampParam = request.getQueryStringParameters().get("timestamp");
+        //ObjectMapper mapper = new ObjectMapper();
+        //System.out.println("Stage: " + System.getenv("Stage"));
+        try {
+            Timestamp timestamp = new Timestamp(Long.getLong(timestampParam));
+            /*if (currentRace != null && curremtRace.status == Status.ARMED) {
+                setCurrentState(Status.STARTED, timestamp);
+
+                return new APIGatewayProxyResponseEvent().withStatusCode(201);
+            }*/
+
+            //return new APIGatewayProxyResponseEvent().withStatusCode(200);
+            return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(timestamp.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new APIGatewayProxyResponseEvent().withStatusCode(500);
+        }
     }
 }
