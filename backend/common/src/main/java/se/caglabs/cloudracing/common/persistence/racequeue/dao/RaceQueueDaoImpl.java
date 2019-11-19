05eb0447e8d109c15ee120f4cd8b20d0eb5cacaf
ccbjab <bjorn.aberg@cag.se>
2019-11-19 09:02:49 +0100

    remove from RaceQueue when bailoutfromrace

--- backend/common/src/main/java/se/caglabs/cloudracing/common/persistence/racequeue/dao/RaceQueueDaoImpl.java
+++ backend/common/src/main/java/se/caglabs/cloudracing/common/persistence/racequeue/dao/RaceQueueDaoImpl.java
@@ -4,11 +4,13 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import se.caglabs.cloudracing.common.persistence.racequeue.model.RaceQueue;
 import se.caglabs.cloudracing.common.persistence.stuff.StageNameTableNameResolver;
 
 
-public class RaceQueueDaoImpl implements RaceQueueDao{
+public class RaceQueueDaoImpl implements RaceQueueDao {
 
     private AmazonDynamoDB client;
     private DynamoDBMapper mapper;
@@ -17,6 +19,17 @@ public class RaceQueueDaoImpl implements RaceQueueDao{
         this.client = AmazonDynamoDBClientBuilder.standard().build();
         this.mapper = new DynamoDBMapper(this.client, DynamoDBMapperConfig.builder()
                 .withTableNameResolver(new StageNameTableNameResolver()).build());
+    }
+
+    @Override
+    public void removeFromQueue(String id) {
+        RaceQueue key = new RaceQueue();
+        key.setRaceId(id);
+        DynamoDBQueryExpression<RaceQueue> queryExpression = new DynamoDBQueryExpression<RaceQueue>()
+                .withHashKeyValues(key);
+        PaginatedQueryList<RaceQueue> query = this.mapper.query(RaceQueue.class, queryExpression);
+        query.stream().findFirst().ifPresent(raceQueue ->
+                this.mapper.delete(raceQueue));
     }
 
     @Override
