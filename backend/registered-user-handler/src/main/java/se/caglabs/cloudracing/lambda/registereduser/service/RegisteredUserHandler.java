8e9ae4dff2091026f838aa9ac5f2dbbe7a4e46ae
nilden <stefan.nilden@gmail.com>
2019-11-27 22:47:54 +0100

    Merged in logic into the one and only lambda for registering users etc

--- backend/registered-user-handler/src/main/java/se/caglabs/cloudracing/lambda/registereduser/service/RegisteredUserHandler.java
+++ backend/registered-user-handler/src/main/java/se/caglabs/cloudracing/lambda/registereduser/service/RegisteredUserHandler.java
@@ -1,78 +1,49 @@
 package se.caglabs.cloudracing.lambda.registereduser.service;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import se.caglabs.cloudracing.common.persistence.digest.PasswordDigest;
-import se.caglabs.cloudracing.common.persistence.registereduser.dao.UserDao;
-import se.caglabs.cloudracing.common.persistence.registereduser.dao.UserDaoImpl;
-import se.caglabs.cloudracing.common.persistence.registereduser.exception.UserDaoException;
-import se.caglabs.cloudracing.common.persistence.registereduser.model.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 public class RegisteredUserHandler {
 
-    private UserDao dao;
+    private static final String REGISTER_USER = "/registered-user";
+    private static final String REGISTER_USERS = "/registered-users";
+    private static final String POST = "POST";
+    private static final String GET = "GET";
+
+    private RegisteredUserService registeredUserService = new RegisteredUserService();
 
     public RegisteredUserHandler(){}
 
-    /**
-     * For testing purpose.
-     */
-     RegisteredUserHandler(UserDao userDao) {
-         this.dao = userDao;
-     }
+    public APIGatewayProxyResponseEvent route(APIGatewayProxyRequestEvent request, Context context) throws JsonProcessingException {
 
-    public APIGatewayProxyResponseEvent registerUser(APIGatewayProxyRequestEvent request) {
-
-        String body = request.getBody();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            User user = mapper.readValue(body, User.class);
-            user.setPassword(PasswordDigest.digest(user.getPassword()));
-            log.info("Creating new user: {}", user.getName());
-            getUserDao().saveUser(user);
-
-            return new APIGatewayProxyResponseEvent().withStatusCode(201).withBody("Created");
-        } catch (JsonProcessingException  e) {
-            return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Bad input values!");
-        } catch (UserDaoException e) {
-            return new APIGatewayProxyResponseEvent().withStatusCode(409).withBody("User already exists!");
+        String path = request.getPath();
+        if(request.getPathParameters() != null) {
+            path = REGISTER_USER;
         }
-    }
 
-    /**
-     * Fetches a user with a specified name/id
-     */
-    public User getRegisteredUser(String name) {
-         return getUserDao().getUser(name);
-    }
-
-    /**
-     * Fetches all registered users
-     */
-    public List getRegisteredUsers() {
-        List users = getUserDao().listUsers();
-
-        return users;
-    }
-
-    /**
-     * Fetches all races that a user that a user has taken part in.
-     */
-    public List getRegisteredUsersInRace() {
-        return new ArrayList();
-    }
-
-    private UserDao getUserDao() {
-        if(this.dao == null) {
-            this.dao = new UserDaoImpl();
+        if(REGISTER_USER.equals(path)) {
+            switch (request.getHttpMethod()) {
+                case POST:
+                    return registeredUserService.registerUser(request);
+                case GET:
+                    return registeredUserService.getRegisteredUser(request);
+                default:
+                    return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Bad request!");
+            }
+        } else if (REGISTER_USERS.equals(path)) {
+                log.info("Request for /registered-users");
+            switch (request.getHttpMethod()) {
+                case GET:
+                    return registeredUserService.getRegisteredUsers();
+                default:
+                    return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Bad request!!");
+            }
         }
-        return this.dao;
+
+        return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Bad request!!!");
     }
 }
---
+++ backend/registered-user-handler/src/main/java/se/caglabs/cloudracing/lambda/registereduser/service/RegisteredUserService.java
@@ -0,0 +1,73 @@
+package se.caglabs.cloudracing.lambda.registereduser.service;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import se.caglabs.cloudracing.common.persistence.digest.PasswordDigest;
+import se.caglabs.cloudracing.common.persistence.registereduser.dao.UserDao;
+import se.caglabs.cloudracing.common.persistence.registereduser.dao.UserDaoImpl;
+import se.caglabs.cloudracing.common.persistence.registereduser.exception.UserDaoException;
+import se.caglabs.cloudracing.common.persistence.registereduser.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class RegisteredUserService {
+    private static final String NAME = "name";
+    private UserDao dao;
+    private ObjectMapper mapper;
+
+    public RegisteredUserService() {
+        mapper = new ObjectMapper();
+
+    }
+
+    APIGatewayProxyResponseEvent registerUser(APIGatewayProxyRequestEvent request) {
+
+        log.info("Registering new user " + request.getBody());
+        String body = request.getBody();
+        try {
+            User user = mapper.readValue(body, User.class);
+            user.setPassword(PasswordDigest.digest(user.getPassword()));
+            getUserDao().saveUser(user);
+            return new APIGatewayProxyResponseEvent().withStatusCode(201).withBody(mapper.writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("Bad input values!");
+        } catch (UserDaoException e) {
+            return new APIGatewayProxyResponseEvent().withStatusCode(409).withBody("User already exists!");
+        }
+    }
+
+    /**
+     * Fetches a user with a specified name/id
+     */
+    APIGatewayProxyResponseEvent getRegisteredUser(APIGatewayProxyRequestEvent request) throws JsonProcessingException {
+        User user = getUserDao().getUser(request.getPathParameters().get(NAME));
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(mapper.writeValueAsString(user));
+    }
+
+    /**
+     * Fetches all registered users
+     */
+     APIGatewayProxyResponseEvent getRegisteredUsers() throws JsonProcessingException {
+        List users = getUserDao().listUsers();
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(mapper.writeValueAsString(users));
+    }
+
+    /**
+     * Fetches all races that a user that a user has taken part in.
+     */
+    public List getRegisteredUsersInRace() {
+        return new ArrayList();
+    }
+
+    private UserDao getUserDao() {
+        if(this.dao == null) {
+            this.dao = new UserDaoImpl();
+        }
+        return this.dao;
+    }
+}
