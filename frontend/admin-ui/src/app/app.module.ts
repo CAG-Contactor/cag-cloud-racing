a765d0020ec408a4241da4287502ccaf91b51f9b
Joel Binnquist <joel.binnquist@gmail.com>
2019-12-09 17:47:52 +0100

    https://github.com/CAG-Contactor/cag-cloud-racing/issues/20 - Admin UI. WIP

--- frontend/admin-ui/src/app/app.module.ts
+++ frontend/admin-ui/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewComponentsModule } from './view-components/view-components.module'
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ViewComponentsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
