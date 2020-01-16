e10996bd03ecbd0a8939e701fefe8add550aa357
nilden <stefan.nilden@mail.com>
2020-01-16 23:13:32 +0100

    Lagt till lambda som lyssnar på events från ConnectionsTable

---
+++ backend/websocket/EchoFunction.js
@@ -0,0 +1,10 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10', region: process.env.AWS_REGION});
+
+exports.handler = function (event, context, callback) {
+    console.log("Receiving event from database!!");
+    console.log(event);
+    console.log(context);
+    callback(null, "done");
+};
