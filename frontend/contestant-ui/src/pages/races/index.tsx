9d15c94ef5cd947c8578fc3780c57211824cbac8
HenrikHummelgren <henrik.hummelgren@gmail.com>
2020-01-25 21:03:34 +0100

    Fixing time for races in contestant UI.

--- frontend/contestant-ui/src/pages/races/index.tsx
+++ frontend/contestant-ui/src/pages/races/index.tsx
@@ -43,8 +43,8 @@ const Races = () => {
                     {races.map((race: any, index) =>
                         <tr key={index}>
                             <td>{race.userName}</td>
-                            <td><Moment format="mm:ss:SSS">{race.finishTime}</Moment></td>
-                            <td><Moment format="mm:ss:SSS">{race.splitTime}</Moment></td>
+                            <td><Moment format="mm:ss:SSS">{race.finishTime - race.startTime}</Moment></td>
+                            <td><Moment format="mm:ss:SSS">{race.splitTime - race.startTime}</Moment></td>
                             <td><Moment format="YYYY-MM-DD HH:mm">{race.startTime}</Moment></td>
                             <td>{getResultText(race.raceStatus)}</td>
                         </tr>)}
